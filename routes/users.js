d5bc04426280539a50a8dcc089d3471660b1305f
joshBeals <alelebealsjoshua@gmail.com>
2021-08-26 17:31:32 +0100

    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// UPDATE USER ROUTE
+
+// DELETE USER ROUTE
+
+// GET USER ROUTE
+
+// GET ALL USERS ROUTE
+
+// GET USER STATS ROUTE
